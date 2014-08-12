8061905445cc40c4eb06fd459be498194ccb12e3
PalTamas <PalTamas@172.16.182.151>
2014-08-12 09:18:17 +0200

    teszt_001

---
+++ HelloOnlineGit/WebContent/helloonlinegit/OnlineGit.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("helloonlinegit.OnlineGit", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf helloonlinegit.OnlineGit
+	*/ 
+	getControllerName : function() {
+		return "helloonlinegit.OnlineGit";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf helloonlinegit.OnlineGit
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
